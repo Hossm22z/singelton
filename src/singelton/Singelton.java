574d5b3fcddd1125c0525404170370caaa1bc29a
Hossm22z <128164954+Hossm22z@users.noreply.github.com>
2023-08-10 20:05:27 +0300

    Add files via upload

---
+++ src/singelton/Singelton.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package singelton;
+
+/**
+ *
+ * @author Acer
+ */
+public class Singelton {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Singelton1 gitSingelton1;
+    }
+    
+}
---
+++ src/singelton/Singelton1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package singelton;
+
+/**
+ *
+ * @author Acer
+ */
+public class Singelton1 {
+    private   Singelton1 Singelton1= new Singelton1();
+    
+    private Singelton1(){
+        
+    }
+    
+    public  Singelton1 gitSingelton1(){
+    
+    return  Singelton1;
+    
+}
+
+}
